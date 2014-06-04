4f757f80bb9c38d515c498ad62d38ea236b6d53a
Hackbright Student <info@hackbrightacademy.com>
2014-06-04 15:12:51 -0700

    initial commit

---
+++ arithmatic.py
@@ -0,0 +1,25 @@
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2 
+
+def divide(num1, num2):
+    return num1 / num2
+
+def square(num1):
+    return num1 * num1
+
+def cube(num1):
+    return num1 * num1 * num1
+
+def power(num1, num2):
+    return num1 ** num2
+
+def mod(num1, num2):
+    return num1 % num2
+
+print add(1, 2), subtract(2,1), multiply(3, 3), divide(4, 2), square(2), cube(2), power(2,2), mod(4,2)
---
+++ ex3.py
@@ -0,0 +1,23 @@
+print " I will now count my chickens:"
+
+#Add two numbers and divide by 6
+print "Hens", 25 + 30 / 6
+# multiply two numbers and then find modulus of 75%4 and subtract result from 100
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7", 5 - 7
+
+print "Oh, that's why it's False."
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 <= -2 
